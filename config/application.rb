a38ea724483f6980628b4f5c27e22937d2787104
Matan Nahmani <matanmatan999new@gmail.com>
2020-11-20 12:55:22 +0200

    rack cors

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,11 @@ module Portfolioapi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    Rails.application.config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'https://www.matannahmani.com/'
+        resource '*', headers: :any, methods: [:get, :post, :patch, :put]
+      end
+    end
   end
 end
