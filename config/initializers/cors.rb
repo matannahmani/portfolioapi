8d774733713a420ddc18adcc7bc38eb8a645f478
Matan Nahmani <matanmatan999new@gmail.com>
2021-01-18 22:39:36 +0200

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,9 +7,7 @@
 Rails.application.config.action_controller.forgery_protection_origin_check = false
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://www.matannahmani.com/'
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    origins '*'
+    resource '*', headers: :any, methods: [:get, :post, :patch, :put]
   end
 end
