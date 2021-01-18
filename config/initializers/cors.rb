d247cfdfd08d994d751bd588289922c187873abb
Matan Nahmani <matanmatan999new@gmail.com>
2021-01-18 21:52:34 +0200

    changed cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://matannahmani.github.io'
+    origins '*'
 
     resource '*',
       headers: :any,
